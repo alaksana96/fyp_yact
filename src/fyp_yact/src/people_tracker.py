33c80cb962668c27e8200f8aa89c7d175a5cc91a
Aufar Laksana <alaksana96@gmail.com>
2019-05-28 12:47:45 +0100

    Hololens Changes: - Fixed display freezing when no detections with use in Hololens

--- src/fyp_yact/src/people_tracker.py
+++ src/fyp_yact/src/people_tracker.py
@@ -4,6 +4,7 @@ import sys, os
 
 from openpose import pyopenpose as op
 
+
 from fyp_yact.msg import BoundingBox, CompressedImageAndBoundingBoxes
 
 from   collections import deque
@@ -59,29 +60,26 @@ class yact_node:
         if( lstDetections ):
             
             for det in lstDetections:
-                
+
                 if det.Class == 'person':
                     lstDets.append([det.xmin, det.ymin, det.xmax, det.ymax])
-        else:
-            lstDets = []
 
-        if self.frameCount % 1 == 0:
-            self.datum.cvInputData = self.img
-            self.opWrapper.emplaceAndPop([self.datum])
 
-            self.matchDetectionAndPose(lstDets, self.datum.poseKeypoints)
+        self.datum.cvInputData = self.img
+        self.opWrapper.emplaceAndPop([self.datum])
 
-            if self.frameCount % 10 == 0:
-                timer         = time.time() - self.timePrev
-                self.timePrev = time.time()
-                self.intFPS   = int(10/timer)
+        
+        self.matchDetectionAndPose(lstDets, self.datum.poseKeypoints)
 
-            # Print FPS
-            cv2.putText(self.img, 'FPS: {}'.format(self.intFPS), (self.img.shape[1] - 100, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
+        if self.frameCount % 10 == 0:
+            timer         = time.time() - self.timePrev
+            self.timePrev = time.time()
+            self.intFPS   = int(10/timer)
 
-            cv2.imshow('yact: people_tracker.py', self.img)
-        else:
-            cv2.imshow('yact: people_tracker.py', self.img)
+        # Print FPS
+        cv2.putText(self.img, 'FPS: {}'.format(self.intFPS), (self.img.shape[1] - 100, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
+
+        cv2.imshow('yact: people_tracker.py', self.img)
 
         cv2.waitKey(3)
 
@@ -93,35 +91,37 @@ class yact_node:
         Matches the Openpose detections with bounding boxes
         '''
 
-        for pose in poses:
-            '''
-            Check a few key positions (https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/doc/media/keypoints_pose_25.png)
-            0: Mid head
-            1: Mid Torso
-            2, 3, 4: Right Shoulder, Elbow, Arm
-            5, 6, 7: Left Shoulder, Elbow, Arm
-            '''
+        if poses.size > 1:
 
-            # Check torso, right/left shoulder
-            torso     = pose[1]
-            rshoulder = pose[2]
-            lshoulder = pose[5]
-            
-            for bbox in detections:
+            for pose in poses:
+                '''
+                Check a few key positions (https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/doc/media/keypoints_pose_25.png)
+                0: Mid head
+                1: Mid Torso
+                2, 3, 4: Right Shoulder, Elbow, Arm
+                5, 6, 7: Left Shoulder, Elbow, Arm
+                '''
+
+                # Check torso, right/left shoulder
+                torso     = pose[1]
+                rshoulder = pose[2]
+                lshoulder = pose[5]
                 
-                if( self.withinBB(bbox, torso[0], torso[1]) or
-                    self.withinBB(bbox, rshoulder[0], rshoulder[1]) or
-                    self.withinBB(bbox, lshoulder[0], lshoulder[1])):
-
-                    if(rshoulder[0] > lshoulder[0]):
-                        # Moving away from camera
-                        cv2.circle(self.img, (torso[0], torso[1]), 9, (179, 16, 191), -1)
-                    else:
-                        # Moving towards camera
-                        cv2.circle(self.img, (torso[0], torso[1]), 9, (0, 0, 255), -1)
-
-                # Once matched, move onto next pose
-                break 
+                for bbox in detections:
+                    
+                    if( self.withinBB(bbox, torso[0], torso[1]) or
+                        self.withinBB(bbox, rshoulder[0], rshoulder[1]) or
+                        self.withinBB(bbox, lshoulder[0], lshoulder[1])):
+    
+                        if(rshoulder[0] > lshoulder[0]):
+                            # Moving away from camera
+                            cv2.circle(self.img, (torso[0], torso[1]), 9, (179, 16, 191), -1)
+                        else:
+                            # Moving towards camera
+                            cv2.circle(self.img, (torso[0], torso[1]), 9, (0, 0, 255), -1)
+    
+                        # Once matched, move onto next pose
+                        break 
 
                 
     def withinBB(self, bbox, x, y):
